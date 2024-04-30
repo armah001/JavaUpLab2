5685fd0fb1f764623945f12f93ced2bf52dbcb53
Armah <nii.tettey@amalitech.com>
2024-04-30 11:31:04 +0000

    updates

--- src/test/java/com/example/upskilllab2/UserServiceTest.java
+++ src/test/java/com/example/upskilllab2/UserServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
 
@@ -21,5 +23,15 @@ public class UserServiceTest {
         User result = mockService.getUserById(1L);
 
         assertEquals(user, result);
+    }
+
+    @Test
+    public void testVerify(){
+        UserService mockService = Mockito.mock(UserService.class);
+        User user = new User();
+        when(mockService.getUserById(1L)).thenReturn(user);
+
+        User result=mockService.getUserById(1L);
+        verify(mockService).getUserById(1L);
     }
 }
